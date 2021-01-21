714f4ed50b8dab59ed28f43b74770d49490fd561
AayanBaig-WhitehatJR <71639274+AayanBaig-WhitehatJR@users.noreply.github.com>
2021-01-21 17:47:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,21 +10,42 @@ console.log(event);
 var content = event.results[0][0].transcript
 console.log(content)
 document.getElementById("textbox").innerHTML = content;
-speak()
+if(content == "take my selfie"){
+    console.log("Taking selfie.");
+    speak()
 }
+}
+
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value
+    speak_data = "Taking your selfie in 5 seconds."
     console.log(speak_data);
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera)   
+    setTimeout(function(){
+        snapPhoto()
+        downloadImage()
+    }, 5000)
+    //snapPhoto()
 }
 
 Webcam.set({
     width:320,
     height:240,
-    image_format:'png',
+    image_format:'png',     
     png_quality:90
 });
+function snapPhoto(){
+    Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML = "<img id = 'final_snap' src = " + data_uri + ">" 
+    }); 
+}
 camera = document.getElementById('camera')
+function downloadImage(){
+    var link = document.getElementById("link");
+    var image = document.getElementById("final_snap").src;
+    link.href = image;
+    link.click();
+    
+}
